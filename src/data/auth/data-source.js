2a08ae99934f13403efbdfbe953144f0a674227d
Michael Neeley <micneeley14@gmail.com>
2019-12-16 16:09:10 -0500

    chore: Adds Better Auth Error Message

--- src/data/auth/data-source.js
+++ src/data/auth/data-source.js
@@ -1,6 +1,33 @@
 import { Auth as baseAuth } from '@apollosproject/data-connector-rock';
+import { AuthenticationError } from 'apollo-server';
 
 export default class Auth extends baseAuth.dataSource {
+  getCurrentPerson = async ({ cookie = null } = { cookie: null }) => {
+    const { rockCookie, currentPerson } = this.context;
+    const userCookie = cookie || rockCookie;
+
+    if (currentPerson) {
+      return currentPerson;
+    }
+
+    if (userCookie) {
+      try {
+        const request = await this.request('People/GetCurrentPerson').get({
+          options: {
+            headers: { cookie: userCookie, 'Authorization-Token': null },
+          },
+        });
+        this.context.currentPerson = request;
+        return request;
+      } catch (e) {
+        throw new AuthenticationError(
+          `Invalid user cookie. User cookie: ${cookie}. Rock error: ${e.message}`
+        );
+      }
+    }
+    throw new AuthenticationError('Must be logged in');
+  };
+
   authenticate = async ({ identity, password }) => {
     // strip off email part of staff logins for Active Directory
     const newspringUsername = identity.includes('@newspring.cc')
