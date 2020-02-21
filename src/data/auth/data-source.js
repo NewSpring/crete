e266f3805ed3b360c52c576612a9fbc08d47b20b
Michael Neeley <micneeley14@gmail.com>
2020-02-20 22:04:31 -0500

    chore: Removes custom code from auth

--- src/data/auth/data-source.js
+++ src/data/auth/data-source.js
@@ -1,71 +1,14 @@
 import { Auth as baseAuth } from '@apollosproject/data-connector-rock';
-import { fetch, Request } from 'apollo-server-env';
-import { AuthenticationError } from 'apollo-server';
 
 export default class Auth extends baseAuth.dataSource {
-  getCurrentPerson = async ({ cookie = null } = { cookie: null }) => {
-    const { rockCookie, currentPerson } = this.context;
-    const userCookie = cookie || rockCookie;
+  baseAuthenticate = baseAuth.authenticate;
 
-    if (currentPerson) {
-      return currentPerson;
-    }
-
-    if (userCookie) {
-      try {
-        const request = await this.request('People/GetCurrentPerson').get({
-          options: {
-            headers: { cookie: userCookie, 'Authorization-Token': null },
-          },
-        });
-        this.context.currentPerson = request;
-        return request;
-      } catch (e) {
-        throw new AuthenticationError(
-          `Invalid user cookie. New cookie: ${cookie}. Existing cookie: ${rockCookie}. Rock error: ${e.message}`
-        );
-      }
-    }
-    throw new AuthenticationError('Must be logged in');
-  };
-
-  fetchUserCookie = async (Username, Password) => {
-    try {
-      // We use `new Response` rather than string/options b/c if conforms more closely with ApolloRESTDataSource
-      // (makes mocking in tests WAY easier to use `new Request` as an input in both places)
-      const response = await fetch(
-        new Request(`${this.baseURL}/Auth/Login`, {
-          method: 'POST',
-          body: JSON.stringify({
-            Username,
-            Password,
-            Persisted: true,
-          }),
-          headers: {
-            'Content-Type': 'Application/Json',
-          },
-        })
-      );
-      if (response.status >= 400) throw new AuthenticationError();
-      const cookie = response.headers.get('set-cookie');
-      return cookie;
-    } catch (err) {
-      throw new AuthenticationError('Invalid Credentials');
-    }
-  };
-
-  authenticate = async ({ identity, password }) => {
+  authenticate = ({ identity, ...creds }) => {
     // strip off email part of staff logins for Active Directory
     const newspringUsername = identity.includes('@newspring.cc')
       ? identity.split('@')[0]
       : identity;
-    const cookie = await this.fetchUserCookie(newspringUsername, password);
-    const sessionId = await this.createSession({ cookie });
-    const token = baseAuth.generateToken({ cookie, sessionId });
-    const { userToken, rockCookie } = baseAuth.registerToken(token);
-    this.context.rockCookie = rockCookie;
-    this.context.userToken = userToken;
-    this.context.sessionId = sessionId;
-    return { token, rockCookie };
+
+    this.baseAuthenticate({ identity: newspringUsername, ...creds });
   };
 }
