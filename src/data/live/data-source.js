9aa0b0c9337a6912fba5b6cc74b65568a56521ff
richarddubay <richardtdubayjr@gmail.com>
2020-03-10 16:24:14 -0400

    get isLive

--- src/data/live/data-source.js
+++ src/data/live/data-source.js
@@ -1,10 +1,23 @@
-import { dataSource as baseLive } from '@apollosproject/data-connector-church-online';
+// import { dataSource as baseLive } from '@apollosproject/data-connector-church-online';
+import RockApolloDataSource from '@apollosproject/rock-apollo-data-source';
+import ApollosConfig from '@apollosproject/config';
 
-export default class LiveStream extends baseLive {
+const { CHURCH_ONLINE, ROCK_MAPPINGS } = ApollosConfig;
+
+export default class LiveStream extends RockApolloDataSource {
+  resource = 'LiveStream';
+
+  mediaUrls = () => {
+    return CHURCH_ONLINE.MEDIA_URLS;
+  };
+
+  webViewUrl = () => {
+    return CHURCH_ONLINE.WEB_VIEW_URL;
+  };
+
   async getLiveStream() {
-    // TODO pull top two fields from Rock schedule
     return {
-      isLive: true,
+      isLive: () => this.getIsLive(),
       eventStartTime: new Date().toJSON(),
       media: () =>
         this.mediaUrls.length
@@ -16,5 +29,28 @@ export default class LiveStream extends baseLive {
           : null,
       webViewUrl: this.webViewUrl,
     };
+  }
+
+  async getLiveStreams() {
+    const { ContentItem } = this.context.dataSources;
+    // This logic is a little funky right now.
+    // The follow method looks at the sermon feed and the `getLiveStream` on this module
+    // If we have data in the sermon feed, and the `getLiveStream.isLive` is true
+    // this returns an array of livestreams
+    const liveItems = await ContentItem.getActiveLiveStreamContent();
+    return Promise.all(
+      liveItems.map(async (item) => ({
+        contentItem: item,
+        ...(await this.getLiveStream()),
+      }))
+    );
+  }
+
+  async getIsLive() {
+    const areWeLive = await this.post(
+      `Lava/RenderTemplate`,
+      `{[ scheduledcontent schedulecategoryid:'${ROCK_MAPPINGS.SCHEDULE_CATEGORY_ID}' showwhen:'both' ]}{{ IsLive }}{[ endscheduledcontent ]}`
+    );
+    return areWeLive === 'true';
   }
 }
