ed7c6c93fece7e390a086c1fc51bd6fb336768e0
Richard Dubay <richardtdubayjr@gmail.com>
2020-04-20 08:19:32 -0400

    chore: Update API to use InteractionsChannelId (#87)
    * Updated interactions component lookup to use InteractionChannelId
    
    * Correctly sets the InteractionDateTime to local time instead of GMT and removes warning about an invalid date
    
    * Added some tests
    
    Co-authored-by: Michael Neeley <micneeley14@gmail.com>

---
+++ src/data/rock-constants/test-utils.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/prefer-default-export */
+export const buildGetMock = (response, dataSource) => {
+  const get = jest.fn();
+  if (Array.isArray(response) && Array.isArray(response[0])) {
+    response.forEach((responseVal) => {
+      get.mockReturnValueOnce(
+        new Promise((resolve) => resolve(dataSource.normalize(responseVal)))
+      );
+    });
+  }
+  get.mockReturnValue(
+    new Promise((resolve) => resolve(dataSource.normalize(response)))
+  );
+  return get;
+};
