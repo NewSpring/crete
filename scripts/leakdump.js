780cbbc7e5dc688ad9c36224dce3370e715fe734
Michael Neeley <micneeley14@gmail.com>
2020-05-22 11:19:39 -0400

    chore: Adds script to capture memory dump to S3 (#107)
    * chore: Added Script to capture memory dumps
    
    * on interval
    
    * add script to start command

---
+++ scripts/leakdump.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const heapdump = require('heapdump');
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const dir = process.cwd();
+
+const dump = () => {
+  const filename = `${dir}/${Date.now()}.heapsnapshot`;
+  heapdump.writeSnapshot(filename, (err, path) => {
+    if (err) throw err;
+    console.log('dump written to', path);
+  });
+  return filename;
+};
+
+const uploadFile = (filename) => {
+  fs.readFile(filename, (err, data) => {
+    if (err) throw err;
+    const params = {
+      Bucket: 'ns.sandbox', // pass your bucket name
+      Key: `crete/${filename.split('/')[filename.split('/').length - 1]}`,
+      Body: data,
+    };
+    s3.upload(params, (s3Err, s3Data) => {
+      if (s3Err) throw s3Err;
+      console.log(`File uploaded successfully at ${s3Data.Location}`);
+    });
+  });
+};
+
+setInterval(() => {
+  const filename = dump();
+  uploadFile(filename);
+}, 60000 * 30);
