f0d708afaeb3d4000604cd4de8693c005e382cc0
Michael Neeley <micneeley14@gmail.com>
2020-07-08 14:12:54 -0400

    get correct image

--- src/data/events/data-source.js
+++ src/data/events/data-source.js
@@ -1,13 +1,18 @@
 import { Event as baseEvent } from '@apollosproject/data-connector-rock';
+import ApollosConfig from '@apollosproject/config';
+
+const { ROCK_MAPPINGS } = ApollosConfig;
 
 export default class Event extends baseEvent.dataSource {
   getImage = async (eventItemId) => {
     const { ContentItem } = this.context.dataSources;
-    const event = await this.request('EventItems')
+    // get corrsponding events on the public calendar
+    const calendarItem = await this.request('EventCalendarItems')
       .cache({ ttl: 60 })
-      .find(eventItemId)
-      .get();
-    if (!Object.keys(event.attributeValues).length) return null;
-    return ContentItem.getImages(event);
+      .filter(`EventItemId eq ${eventItemId}`)
+      .andFilter(`EventCalendarId eq ${ROCK_MAPPINGS.PUBLIC_CALENDAR_ID}`)
+      .first();
+    if (!Object.keys(calendarItem.attributeValues).length) return null;
+    return ContentItem.getCoverImage(calendarItem);
   };
 }
