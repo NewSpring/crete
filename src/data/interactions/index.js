c7e2f21b177887146e9f3e973a09be4fcffe8d68
Michael Neeley <micneeley14@gmail.com>
2020-07-15 15:06:11 -0400

    a little closer

--- src/data/interactions/index.js
+++ src/data/interactions/index.js
@@ -1,4 +1,4 @@
-import { parseGlobalId } from '@apollosproject/server-core';
+import { createGlobalId } from '@apollosproject/server-core';
 import { Interactions as baseInteractions } from '@apollosproject/data-connector-rock';
 import Config from '@apollosproject/config';
 
@@ -19,25 +19,30 @@ class Interactions extends baseInteractions.dataSource {
     const {
       dataSources: { RockConstants, Auth },
     } = this.context;
-    if (!(__type in ROCK_MAPPINGS.INTERACTIONS.CHANNEL_IDS)) return null;
-    const interactionComponent = RockConstants.createOrFindInteractionComponent(
-      {
-        componentName: `${ROCK_MAPPINGS.INTERACTIONS.COMPONENT_NAME} - ${id}`,
-        channelId: ROCK_MAPPINGS.INTERACTIONS.CHANNEL_IDS[__type],
-        entityId: parseInt(id, 10),
-      }
-    );
-    // TODO get the right interaction component complete with the right channel
-    // based on the content item type
+    const {
+      title: componentName,
+      contentChannelId: channelId,
+    } = await this.request('ContentChannelItems')
+      .find(id)
+      .cache({ ttl: 86400 })
+      .get();
+    // TODO convert content channel ID to interaction channel ID
+    const {
+      id: componentId,
+    } = await RockConstants.createOrFindInteractionComponent({
+      componentName,
+      channelId,
+      entityId: parseInt(id, 10),
+    });
     const currentUser = await Auth.getCurrentPerson();
     // await this.post('/Interactions', {
     // PersonAliasId: currentUser.primaryAliasId,
-    // InteractionComponentId: interactionComponent.id,
-    // InteractionSessionId: this.context.sessionId,
+    // InteractionComponentId: componentId,
     // Operation: 'VIEW',
     // InteractionDateTime: new Date().toJSON(),
     // InteractionSummary: 'VIEW',
-    // ForeignKey: id,
+    // ForeignKey: createGlobalId(id, __type),
+    // ChannelCustom1: 'corinth',
     // });
     return null;
   }
