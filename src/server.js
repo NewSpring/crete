dc9fe8f0dc20c0c777e113797afe374cbd306e26
Michael Neeley <micneeley14@gmail.com>
2020-07-15 15:14:32 -0400

    chore: Adds Bugsnag Tracking

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ import { ApolloServer } from 'apollo-server-express';
 import ApollosConfig from '@apollosproject/config';
 import express from 'express';
 import { RockLoggingExtension } from '@apollosproject/rock-apollo-data-source';
-import { bugsnagMiddleware } from './bugsnag';
+import { get, fromPairs } from 'lodash';
+import bugsnag, { bugsnagMiddleware } from './bugsnag';
 // TODO find out what this is
 // import { setupUniversalLinks } from '@apollosproject/server-core';
 import {
@@ -34,6 +35,54 @@ const cacheOptions = isDev
 
 const { ENGINE } = ApollosConfig;
 
+// NOTE this is an effort to find the auth bug, this can come out once we have it.
+const report = (error, metaData, beforeSend) => {
+  bugsnag.notify(error, {
+    metaData: {
+      Rock: { rockUrl: ApollosConfig.ROCK.API_URL },
+      ...metaData,
+    },
+    beforeSend,
+  });
+};
+const plugins = [
+  {
+    requestDidStart() {
+      return {
+        didEncounterErrors({ errors, request }) {
+          const headers = fromPairs(Array.from(request.http.headers.entries()));
+          errors.forEach((error) => {
+            isDev && console.log(error); // eslint-disable-line
+            report(
+              error,
+              {
+                'GraphQL Info': {
+                  query: request.query,
+                  location: JSON.stringify(error.locations),
+                  variables: request.variables,
+                  operationName: request.operationName,
+                  headers,
+                },
+                'Auth Error Info': get(
+                  error,
+                  'extensions.exception.userContext'
+                ),
+              },
+              (err) => {
+                const ip = get(headers, 'fastly-client-ip', 'unknown');
+                err.user = { // eslint-disable-line
+                  id: ip,
+                  appVersion: get(headers, 'user-agent', 'unknown'),
+                };
+              }
+            );
+          });
+        },
+      };
+    },
+  },
+];
+
 const apolloServer = new ApolloServer({
   typeDefs: schema,
   resolvers,
@@ -41,6 +90,7 @@ const apolloServer = new ApolloServer({
   context,
   introspection: true,
   extensions,
+  plugins,
   debug: true,
   playground: {
     settings: {
