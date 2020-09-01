68ff29e9ce30b96f45acb07450b1fe28822f4d14
richarddubay <richardtdubayjr@gmail.com>
2020-09-01 16:51:38 -0400

    Changed query to pass the Rock prayer id and not the node id

--- src/data/prayers/resolver.js
+++ src/data/prayers/resolver.js
@@ -8,8 +8,8 @@ import { createAssetUrl } from '../utils';
 
 export default {
   Query: {
-    prayer: (root, { id }, { dataSources }) =>
-      dataSources.Prayer.getFromId(parseGlobalId(id).id),
+    // This takes a Rock ID, not a Node ID.
+    prayer: (root, { id }, { dataSources }) => dataSources.Prayer.getFromId(id),
     // deprecated
     prayers: (root, { type }, { dataSources }) =>
       dataSources.Prayer.getPrayers(type),
