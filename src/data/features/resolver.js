1d64f1a6367a9de7bf308a84eea1b562d1af7a7b
Vincent Wilson <vince@classyh.at>
2021-09-12 09:28:45 -0400

    Patch in missing resolver

--- src/data/features/resolver.js
+++ src/data/features/resolver.js
@@ -1,5 +1,5 @@
 import { Feature } from '@apollosproject/data-connector-rock';
-import { resolverMerge } from '@apollosproject/server-core';
+import { resolverMerge, createGlobalId } from '@apollosproject/server-core';
 
 const resolver = {
   CardListItem: {
@@ -7,6 +7,48 @@ const resolver = {
   },
   ActionListAction: {
     subtitle: () => null,
+  },
+  // This was moved from data-connector-rock to data-connector-postgres
+  // There are no specific overides.
+  // We should delete it as soon as we bump the postgres package version.
+  AddCommentFeature: {
+    initialPrompt: ({ id, data }) =>
+      data?.initialPrompt || JSON.parse(id).initialPrompt,
+    addPrompt: ({ id, data }) => data?.addPrompt || JSON.parse(id).addPrompt,
+    relatedNode: async (
+      { id, parentType, parentId },
+      args,
+      { models: { Node }, dataSources },
+      info
+    ) => {
+      if (parentType && parentId) {
+        return Node.get(
+          createGlobalId(parentId, parentType),
+          dataSources,
+          info
+        );
+      }
+      const { relatedNode } = JSON.parse(id);
+      return Node.get(
+        createGlobalId(relatedNode.id, relatedNode.__type),
+        dataSources,
+        info
+      );
+    },
+  },
+  CommentListFeature: {
+    comments: ({ id, data, parentType, parentId }, args, { dataSources }) => {
+      // Old mechanism, based on Rock.
+      if (!data) {
+        return dataSources.Comment.getForNode(JSON.parse(id));
+      }
+      // New mechanism, based on Postgres
+      return dataSources.Comment.getForNode({
+        parentId,
+        parentType,
+        flagLimit: data.flagLimit,
+      });
+    },
   },
 };
 
